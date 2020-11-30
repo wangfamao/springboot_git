30311e83b6a6e95fa01b360778f84634218237d2
wangfamao <wangfamao@163.com>
2020-11-30 16:29:06 +0800

    获取服务器端口

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+    @GetMapping("/getPort")
+    public String getPort(){
+        return "服务器端口："+port;
     }
 }
