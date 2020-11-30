a7e850929fa73fcbb21ee140c3eefb337df7c212
wangfamao <wangfamao@163.com>
2020-11-30 15:35:56 +0800

    新增IndexController类

---
+++ src/main/java/com/entor/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.entor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "Hello,Index";
+    }
+}
