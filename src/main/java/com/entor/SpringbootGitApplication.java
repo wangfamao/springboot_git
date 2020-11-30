299b154152960dde80b107cd3d24bd07568ed1d9
wangfamao <wangfamao@163.com>
2020-11-30 15:20:39 +0800

    Initial commit

---
+++ src/main/java/com/entor/SpringbootGitApplication.java
@@ -0,0 +1,13 @@
+package com.entor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entor/SpringbootGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
